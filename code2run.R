c681705787b1e3006132746e8d49cb08e9f0fb8e
Shannon Albeke <salbeke@uwyo.edu>
2020-12-17 18:46:25 -0700

    Tidy up this script

--- code2run.R
+++ code2run.R
@@ -20,116 +20,55 @@ box_fldr <- "E:/Box Sync/KnowledgeIBM_results"
 # github_fldr <- ".../GitHub/KnowledgeIBM"
 
 # Create/Connect to SQLite DB for storage of model runs
-dbConn <- dbConnect(SQLite(), paste0(box_fldr, "/KnowledgeIBM_ModelRuns.sqlite"))
+# dbConn <- dbConnect(SQLite(), paste0(box_fldr, "/KnowledgeIBM_ModelRuns.sqlite"))
+dbConn <- dbConnect(SQLite(), paste0(box_fldr, "/KnowledgeIBM_ModelTuning_20201215.sqlite"))
+dbConnSup <- dbConnect(SQLite(), paste0(box_fldr, "/KnowledgeIBM_ModelTuningSupplemental_20201215.sqlite"))
+
 
 #source the functions
 source(paste0(github_fldr, "/info.transfer.IBM.R"))
 
 #### prepare test data ####
 
-# K selected example
-d <- data.frame(age=c(0,1,2,3),
-                birthRate=c(0,1.4,1.9,1.75),
-                survivalRate=c(0.5,0.95,0.85,0))
-
-# R selected example
-d <- data.frame(age=c(0,1,2,3),
-                birthRate=c(0,14,16,12),
-                survivalRate=c(0.14,0.18,0.22,0))
-
-
-# calculate stable age distribution based on d and N0
-K <- odiag(d$survivalRate[1:(nrow(d)-1)],-1) # Px, survival rates (note, the survival rate in final age class is 0 automatically)
-K[1,] <- d$birthRate # Fertility
-stbl_age <- eigen.analysis(K)$stable.age
-d$N0_proportion <- c(0,stbl_age[2:length(stbl_age)])
-d$N0_proportion <- stbl_age
-d$N0_prob_knowing <- c(0,.05,.1,.15)   # add the probabilities of having information at the beginning of the simulation
-d
-eigen.analysis(K)$lambda1
-rm(K, stbl_age)
-
-# Store the above parameters as well as others so we can track different model runs more easily
-paramDF<- data.frame(Scenario = "test K selected",
-                     vitalRates = nest(d),
-                     N0 = 50,
-                     carryCapcity = 500,
-                     years = 50,
-                     sexRatio = 0.5,
-                     boldDist = nest(data.frame(shape1 = 2, shape2 = 5)),
-                     poisLambda = 2,
-                     infoTransfer = 0.01,
-                     infoDeath = 0.2,
-                     naiveLearn = 0.01,
-                     vertTransmission = TRUE) %>% 
-  rename(vitalRates = data, 
-         boldDist = data.1)
-
 
 #### Model Run ####
-
-d = data.frame(paramDF$vitalRates[1])
-N0 = paramDF$N0[1] 
-K = paramDF$carryCapcity[1]
-yrs = paramDF$years[1] 
-sex.ratio = paramDF$sexRatio[1]
-bold.distr.beta = as.numeric(data.frame(paramDF$boldDist[1]))
-si = paramDF$poisLambda[1] 
-infotransfer = paramDF$infoTransfer[1]
-h = paramDF$infoDeath[1] 
-nl = paramDF$naiveLearn[1]
-vertTransmission = paramDF$vertTransmission[1]
-result.folder = paste0(box_fldr,"/practice") 
-set_seed = FALSE
-save_at_each_iter = FALSE 
-
+paramDF<- dbSendQuery(dbConn, "SELECT * FROM tblModelScenarios") %>% 
+  dbFetch() %>% 
+  mutate(vertTransmission = case_when(vertTransmission == 1 ~ TRUE,
+                                      TRUE ~ FALSE))
 
 # Set up the multithread
-cl<- makeCluster(16)
+cl<- makeCluster(24)
 registerDoParallel(cl)
 
 # Initialize the number of iterations of the same model
-boot<- 500
+boot<- 50
 
 # Loop through the data.frame of model parameters for each scenario
+# Failed = c(12)
 for(i in 1:nrow(paramDF)){
   df<- paramDF[i, ]
   # Now parallelize the individual model runs
   outDF<- foreach(j = 1:boot, .packages = c("tidyverse", "Matrix", "jsonlite")) %dopar% {
     # Run the function (test)
-    result <- info.transfer.IBM(
-      d = data.frame(df$vitalRates[1]),   # these are the starting params of the population including birth and death rates. must have 5 columns: "age","birthRate","survivalRate","N0_proportion","N0_prob_knowing"
-      N0 = df$N0[1], # starting number of individuals
-      K = df$carryCapcity[1], # carrying capacity
-      yrs = df$years[1], # how many years should the simulation run for?
-      sex.ratio = df$sexRatio[1], #what is the sex ratio of of the population/births?
-      bold.distr.beta = as.numeric(data.frame(df$boldDist[1])), # starting probability distribution of being bold, beta distribution (vector of 2 values: shape1 and shape2)
-      si = df$poisLambda[1], # lambda of poison distribution, representing maximum number of interactions between two individuals if both have a boldness of 1. if both animals have boldness of 0, then there will be no interactions
-      infotransfer = df$infoTransfer[1], # given an interaction, what is the probability that information is transferred (min=0, max=1)
-      h = df$infoDeath[1], #increase in probability of death for uninformed, as a proportion of death rate.
-      nl = df$naiveLearn[1], # naive learning probability of the oldest animals (i.e., the ones that have the highest naive learning)
-      vertTransmission = df$vertTransmission[1], # When giving birth, should your information status be given to your offspring? TRUE/FALSE 
-      result.folder = paste0(box_fldr,"/practice"), #an empty folder where results will be saved.
-      set_seed = FALSE, # want to make results reproducible? Then set as TRUE
-      save_at_each_iter = FALSE #should all results be written to file at each time step?
-    )
+    result <- info.transfer.IBM(d = fromJSON(df$vitalRates[1]),N0 = df$N0[1], K = df$carryCapcity[1], yrs = df$years[1], sex.ratio = df$sexRatio[1], bold.distr.beta = as.numeric(fromJSON(df$boldDist[1])), si = df$poisLambda[1], infotransfer = df$infoTransfer[1], h = df$infoDeath[1], nl = df$naiveLearn[1], vertTransmission = df$vertTransmission[1], set_seed = FALSE)
     
     # write the data.frame out to the database
     outStats<- result$PopStats %>% 
-      mutate(Scenario = df$Scenario,
+      mutate(Scenario = df$Scenario[1],
              ModelRun = j) %>% 
       select(Scenario, ModelRun, `time.stamp`:`med.age`)
     
     # converting to dataframe for each matrix for Interactions
-    dfInt <- lapply(1:length(result$Interactions), function(i)
-      cbind(as.data.frame(as.matrix(result$Interactions[[i]]))))
+    dfInt <- lapply(1:length(result$Interactions), function(ii)
+      cbind(as.data.frame(as.matrix(result$Interactions[[ii]]))))
     
     intDF<- data.frame()
     for(k in 1:length(dfInt)){
-      intDF<- rbind(intDF, data.frame(Scenario = df$Scenario,
+      intDF<- rbind(intDF, data.frame(Scenario = df$Scenario[1],
                                       ModelRun = j,
                                       yr = (k - 1),
-                                      Interactions = as.character(toJSON(dfInt[[i]]))))
+                                      Interactions = as.character(toJSON(dfInt[[k]]))))
     }
     
     
@@ -138,7 +77,7 @@ for(i in 1:nrow(paramDF)){
       cbind(as.data.frame(result$Individuals[[i]])))
     
     dfInd<- do.call(rbind, dfInd) %>% 
-      mutate(Scenario = df$Scenario,
+      mutate(Scenario = df$Scenario[1],
              ModelRun = j,
              ID = 1:n()) %>% 
       select(Scenario, ModelRun, ID, alive:birthYr)
@@ -163,8 +102,10 @@ for(i in 1:nrow(paramDF)){
   }
   # Write the tables to the database
   dbWriteTable(dbConn, name = "tblModelStats", value = wStat, append = TRUE)
-  dbWriteTable(dbConn, name = "tblInteractions", value = wInt, append = TRUE)
-  dbWriteTable(dbConn, name = "tblIndividuals", value = wInd, append = TRUE)
+  # dbWriteTable(dbConnSup, name = "tblInteractions", value = wInt, append = TRUE)
+  # dbWriteTable(dbConnSup, name = "tblIndividuals", value = wInd, append = TRUE)
+  
+  print(paste0("Finished with ", i, " of ", nrow(paramDF), ", Scenario: ", df$Scenario[1], " at ", Sys.time()))
 }
 # Stop multithread
 stopCluster(cl)
